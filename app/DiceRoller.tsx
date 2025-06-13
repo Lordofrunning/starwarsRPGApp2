307e72a21685915afec905d6a4365c73b952e219
Trent Chipman <trentchip@gmail.com>
2025-06-12 23:15:58 -0600

    Updates to Dice roller

--- app/DiceRoller.tsx
+++ app/DiceRoller.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+import { default as React, useState } from 'react';
 import { Button, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
+import { styles } from './index.styles';
 const diceStyles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 10,
-    backgroundColor: '#111',
+    backgroundColor: '#FFFFFF',
   },
   diceSelector: {
   flexDirection: 'row',
@@ -69,126 +70,113 @@ resultsWrapper: {
 },
 
 poolIcon: {
-  width: 30,
-  height: 30,
+  width: 40,
+  height: 40,
   margin: 4,
-  borderWidth: 2,
-  borderColor: '#555',
-  borderRadius: 8,
+  borderWidth: 0,
+  borderColor: '#FFFFFF',
+  borderRadius: 0,
 },
 
 resultIcon: {
-  width: 30,
-  height: 30,
+  width: 40,
+  height: 40,
   margin: 4,
-  borderWidth: 1,
-  borderColor: '#444',
-  borderRadius: 6,
+  borderWidth: 0,
+  borderColor: '#FFFFFF',
+  borderRadius: 0,
 },
 });
 
 
 const greenDie = [
-  require('../assets/dice/green/side1_blank.jpg'),
-  require('../assets/dice/green/side2_1advantage.jpg'),
-  require('../assets/dice/green/side3_2suc.jpg'),
-  require('../assets/dice/green/side4_1suc.jpg'),
-// bottom of the dice
-    require('../assets/dice/green/side5_2advantage.jpg'),
-    require('../assets/dice/green/side6_1advantage.jpg'),
-    require('../assets/dice/green/side7_1ad_1suc.jpg'),
-    require('../assets/dice/green/side8_1suc.jpg'),
+  { src: require('../assets/dice/green/side1_blank.jpg'), characters: {} },
+  { src: require('../assets/dice/green/side2_1advantage.jpg'), characters: { advantage: 1 } },
+  { src: require('../assets/dice/green/side3_2suc.jpg'), characters: { success: 2 } },
+  { src: require('../assets/dice/green/side4_1suc.jpg'), characters: { success: 1 } },
+  { src: require('../assets/dice/green/side5_2advantage.jpg'), characters: { advantage: 2 } },
+  { src: require('../assets/dice/green/side6_1advantage.jpg'), characters: { advantage: 1 } },
+  { src: require('../assets/dice/green/side7_1ad_1suc.jpg'), characters: { success: 1, advantage: 1 } },
+  { src: require('../assets/dice/green/side8_1suc.jpg'), characters: { success: 1 } },
 ];
 
 const advantageDie = [
-  require('../assets/dice/blue/side1_blank.jpg'),
-  require('../assets/dice/blue/side2_blank.jpg'),
-  require('../assets/dice/blue/side3_1suc.jpg'),
-  require('../assets/dice/blue/side4_2ad.jpg'),
-// bottom of the dice
-    require('../assets/dice/blue/side5_1advantage.jpg'),
-    require('../assets/dice/blue/side6_1ad_1suc.jpg'),
-  
+  { src: require('../assets/dice/blue/side1_blank.jpg'), characters: {} },
+  { src: require('../assets/dice/blue/side2_blank.jpg'), characters: {} },
+  { src: require('../assets/dice/blue/side3_1suc.jpg'), characters: { success: 1 } },
+  { src: require('../assets/dice/blue/side4_2ad.jpg'), characters: { advantage: 2 } },
+  { src: require('../assets/dice/blue/side5_1advantage.jpg'), characters: { advantage: 1 } },
+  { src: require('../assets/dice/blue/side6_1ad_1suc.jpg'), characters: { success: 1, advantage: 1 } },
 ];
 
 const yellowDie = [
-     require('../assets/dice/yellow/side7_blank.jpg'),
-  
-  require('../assets/dice/yellow/side2_1advantage.jpg'),
-  require('../assets/dice/yellow/side3_2suc.jpg'),
-  require('../assets/dice/yellow/side4_1suc.jpg'),
-    require('../assets/dice/yellow/side5_2advantage.jpg'),
-    require('../assets/dice/yellow/side6_1ad_1suc.jpg'),
-    // bottom of the dice
-   require('../assets/dice/yellow/side1_triumph.jpg'),
-    require('../assets/dice/yellow/side8_2suc.jpg'),
-    require('../assets/dice/yellow/side9_1ad_1suc.jpg'),
-  require('../assets/dice/yellow/side10_1ad_1suc.jpg'),
-  require('../assets/dice/yellow/side11_2ad.jpg'),
-  require('../assets/dice/yellow/side12_1suc.jpg'),
-
+  { src: require('../assets/dice/yellow/side7_blank.jpg'), characters: {} },
+  { src: require('../assets/dice/yellow/side2_1advantage.jpg'), characters: { advantage: 1 } },
+  { src: require('../assets/dice/yellow/side3_2suc.jpg'), characters: { success: 2 } },
+  { src: require('../assets/dice/yellow/side4_1suc.jpg'), characters: { success: 1 } },
+  { src: require('../assets/dice/yellow/side5_2advantage.jpg'), characters: { advantage: 2 } },
+  { src: require('../assets/dice/yellow/side6_1ad_1suc.jpg'), characters: { success: 1, advantage: 1 } },
+  { src: require('../assets/dice/yellow/side1_triumph.jpg'), characters: { success: 1, triumph: 1 } },  // Triumph assumed
+  { src: require('../assets/dice/yellow/side8_2suc.jpg'), characters: { success: 2 } },
+  { src: require('../assets/dice/yellow/side9_1ad_1suc.jpg'), characters: { success: 1, advantage: 1 } },
+  { src: require('../assets/dice/yellow/side10_1ad_1suc.jpg'), characters: { success: 1, advantage: 1 } },
+  { src: require('../assets/dice/yellow/side11_2ad.jpg'), characters: { advantage: 2 } },
+  { src: require('../assets/dice/yellow/side12_1suc.jpg'), characters: { success: 1 } },
 ];
 
-
 const purpleDie = [
-  require('../assets/dice/purple/side1_blank.jpg'),
-  require('../assets/dice/purple/side2_1thr_1fail.jpg'),
-  require('../assets/dice/purple/side3_1thr.jpg'),
-  require('../assets/dice/purple/side4_2fail.jpg'),
-// bottom of the dice
-    require('../assets/dice/purple/side5_2thr.jpg'),
-    require('../assets/dice/purple/side6_1thr.jpg'),
-    require('../assets/dice/purple/side7_1fail.jpg'),
-    require('../assets/dice/purple/side8_1thr.jpg'),
+  { src: require('../assets/dice/purple/side1_blank.jpg'), characters: {} },
+  { src: require('../assets/dice/purple/side2_1thr_1fail.jpg'), characters: { threat: 1, failure: 1 } },
+  { src: require('../assets/dice/purple/side3_1thr.jpg'), characters: { threat: 1 } },
+  { src: require('../assets/dice/purple/side4_2fail.jpg'), characters: { failure: 2 } },
+  { src: require('../assets/dice/purple/side5_2thr.jpg'), characters: { threat: 2 } },
+  { src: require('../assets/dice/purple/side6_1thr.jpg'), characters: { threat: 1 } },
+  { src: require('../assets/dice/purple/side7_1fail.jpg'), characters: { failure: 1 } },
+  { src: require('../assets/dice/purple/side8_1thr.jpg'), characters: { threat: 1 } },
 ];
 
 const setbackDie = [
-  require('../assets/dice/black/side1_blank.jpg'),
-  require('../assets/dice/black/side2_blank.jpg'),
-  require('../assets/dice/black/side3_1fail.jpg'),
-  require('../assets/dice/black/side4_1thr.jpg'),
-// bottom of the dice
-    require('../assets/dice/black/side5_1fail.jpg'),
-    require('../assets/dice/black/side6_1thr.jpg'),
-  
+  { src: require('../assets/dice/black/side1_blank.jpg'), characters: {} },
+  { src: require('../assets/dice/black/side2_blank.jpg'), characters: {} },
+  { src: require('../assets/dice/black/side3_1fail.jpg'), characters: { failure: 1 } },
+  { src: require('../assets/dice/black/side4_1thr.jpg'), characters: { threat: 1 } },
+  { src: require('../assets/dice/black/side5_1fail.jpg'), characters: { failure: 1 } },
+  { src: require('../assets/dice/black/side6_1thr.jpg'), characters: { threat: 1 } },
 ];
 
 const redDie = [
-    require('../assets/dice/red/side7_blank.jpg'),
-  require('../assets/dice/red/side2_1thr.jpg'),
-  require('../assets/dice/red/side3_1fail.jpg'),
-  require('../assets/dice/red/side4_2thr.jpg'),
-    require('../assets/dice/red/side5_1thr_1fail.jpg'),
-    require('../assets/dice/red/side6_2fail.jpg'),
-    // bottom of the dice
-    require('../assets/dice/red/side1_despair.jpg'),
-    require('../assets/dice/red/side8_2fail.jpg'),
-    require('../assets/dice/red/side9_1thr_1fail.jpg'),
-  require('../assets/dice/red/side10_2thr.jpg'),
-  require('../assets/dice/red/side11_1fail.jpg'),
-  require('../assets/dice/red/side12_1thr.jpg'),
-
+  { src: require('../assets/dice/red/side7_blank.jpg'), characters: {} },
+  { src: require('../assets/dice/red/side2_1thr.jpg'), characters: { threat: 1 } },
+  { src: require('../assets/dice/red/side3_1fail.jpg'), characters: { failure: 1 } },
+  { src: require('../assets/dice/red/side4_2thr.jpg'), characters: { threat: 2 } },
+  { src: require('../assets/dice/red/side5_1thr_1fail.jpg'), characters: { failure: 1, threat: 1 } },
+  { src: require('../assets/dice/red/side6_2fail.jpg'), characters: { failure: 2 } },
+  { src: require('../assets/dice/red/side1_despair.jpg'), characters: { failure: 1, despair: 1 } },  // Despair assumed
+  { src: require('../assets/dice/red/side8_2fail.jpg'), characters: { failure: 2 } },
+  { src: require('../assets/dice/red/side9_1thr_1fail.jpg'), characters: { failure: 1, threat: 1 } },
+  { src: require('../assets/dice/red/side10_2thr.jpg'), characters: { threat: 2 } },
+  { src: require('../assets/dice/red/side11_1fail.jpg'), characters: { failure: 1 } },
+  { src: require('../assets/dice/red/side12_1thr.jpg'), characters: { threat: 1 } },
 ];
 
 const forceDie = [
-  require('../assets/dice/force/side1_1ds.jpg'),
-  require('../assets/dice/force/side2_2ls.jpg'),
-  require('../assets/dice/force/side3_1ls.jpg'),
-  require('../assets/dice/force/side4_2ds.jpg'),
-    require('../assets/dice/force/side5_1ds.jpg'),
-    require('../assets/dice/force/side6_1ds.jpg'),
-    // bottom of the dice
-    require('../assets/dice/force/side7_1ds.jpg'),
-    require('../assets/dice/force/side8_2ls.jpg'),
-    require('../assets/dice/force/side9_1ls.jpg'),
-  require('../assets/dice/force/side10_2ls.jpg'),
-  require('../assets/dice/force/side11_1ds.jpg'),
-  require('../assets/dice/force/side12_1ds.jpg'),
-
+  { src: require('../assets/dice/force/side1_1ds.jpg'), characters: { dark: 1 } },
+  { src: require('../assets/dice/force/side2_2ls.jpg'), characters: { light: 2 } },
+  { src: require('../assets/dice/force/side3_1ls.jpg'), characters: { light: 1 } },
+  { src: require('../assets/dice/force/side4_2ds.jpg'), characters: { dark: 2 } },
+  { src: require('../assets/dice/force/side5_1ds.jpg'), characters: { dark: 1 } },
+  { src: require('../assets/dice/force/side6_1ds.jpg'), characters: { dark: 1 } },
+  { src: require('../assets/dice/force/side7_1ds.jpg'), characters: { dark: 1 } },
+  { src: require('../assets/dice/force/side8_2ls.jpg'), characters: { light: 2 } },
+  { src: require('../assets/dice/force/side9_1ls.jpg'), characters: { light: 1 } },
+  { src: require('../assets/dice/force/side10_2ls.jpg'), characters: { light: 2 } },
+  { src: require('../assets/dice/force/side11_1ds.jpg'), characters: { dark: 1 } },
+  { src: require('../assets/dice/force/side12_1ds.jpg'), characters: { dark: 1 } },
 ];
 
+
 // Assuming you've already imported diceTypes and declared the types
-type DieType = 'green' | 'yellow' | 'purple' | 'red' | 'blue' | 'black' | 'force';
+type DieType = 'Ability' | 'Proficiency' | 'Difficulty' | 'Challenge' | 'Advantage' | 'Setback' | 'force';
 
 interface DicePoolItem {
   type: DieType;
@@ -196,22 +184,22 @@ interface DicePoolItem {
 
 // You'll also need this where your dice arrays are set up
 const diceTypes: Record<DieType, any[]> = {
-  green: greenDie,
-  yellow: yellowDie,
-  purple: purpleDie, // Make sure the import matches
-  red: redDie,
-  blue: advantageDie,
-  black: setbackDie,
+  Ability: greenDie,
+  Proficiency: yellowDie,
+  Difficulty: purpleDie, // Make sure the import matches
+  Challenge: redDie,
+  Advantage: advantageDie,
+  Setback: setbackDie,
   force: forceDie,
 };
 
 const diceIcons: Record<DieType, any> = {
-  green: require('../assets/dice/GreenDiceFull.jpg'),
-  yellow: require('../assets/dice/YellowDiceFull.jpg'),
-  purple: require('../assets/dice/PurpleDiceFull.jpg'),
-  red: require('../assets/dice/RedDiceFull.jpg'),
-  blue: require('../assets/dice/BlueDiceFull.jpg'),
-  black: require('../assets/dice/BlackDiceFull.jpg'),
+  Ability: require('../assets/dice/GreenDiceFull.jpg'),
+  Proficiency: require('../assets/dice/YellowDiceFull.jpg'),
+  Difficulty: require('../assets/dice/PurpleDiceFull.jpg'),
+  Challenge: require('../assets/dice/RedDiceFull.jpg'),
+  Advantage: require('../assets/dice/BlueDiceFull.jpg'),
+  Setback: require('../assets/dice/BlackDiceFull.jpg'),
   force: require('../assets/dice/ForceDiceFull.jpg'),
 };
 
@@ -221,6 +209,7 @@ const diceIcons: Record<DieType, any> = {
 
 
 export default function DiceRoller() {
+  const router = useRouter();
   const [dicePool, setDicePool] = useState<DicePoolItem[]>([]);
   const [results, setResults] = useState<any[]>([]);
 
@@ -234,7 +223,7 @@ export default function DiceRoller() {
   const rollDice = () => {
     const newResults = dicePool.map(die => {
       const sides = diceTypes[die.type];
-      const randomIndex = Math.floor(Math.random() * sides.length);
+      const randomIndex = Math.floor(Math.random() * sides.length);//I question how this works correctly
       return sides[randomIndex];
     });
     setResults(newResults);
@@ -246,6 +235,30 @@ export default function DiceRoller() {
   };
 
   return (
+       <View style={{ flex: 1, position: 'relative', backgroundColor: '#D3D3D3' }}>
+      <View style={styles.container}>
+        <Stack.Screen options={{ headerShown: false }} />
+    {/* Header */}
+          <View style={styles.header}>
+            <TouchableOpacity onPress={() => router.back()} style={styles.sideButton}>
+              <Text style={styles.menuArrow}>←</Text>
+            </TouchableOpacity>
+    
+            <View style={styles.logoContainer}>
+              <Image
+                source={require('../assets/images/logos/rpg_main_logo.png')}
+                style={styles.smallImage}
+                resizeMode="contain"
+              />
+            </View>
+    
+            <TouchableOpacity onPress={() => console.log('Profile pressed')} style={styles.sideButton}>
+              <Image
+                source={require('../assets/images/empty_profile_pic.png')}
+                style={styles.profileImage}
+              />
+            </TouchableOpacity>
+          </View>
     <View style={diceStyles.container}>
       <View style={diceStyles.diceSelector}>
   {Object.keys(diceTypes).map(type => (
@@ -269,7 +282,7 @@ export default function DiceRoller() {
     <View style={diceStyles.poolDiceWrapper}>
       {dicePool.map((die, index) => (
         <TouchableOpacity key={index} onPress={() => removeDieFromPool(index)}>
-          <Image source={diceTypes[die.type][0]} style={diceStyles.poolIcon} />
+          <Image source={diceTypes[die.type][0].src} style={diceStyles.poolIcon} />
         </TouchableOpacity>
       ))}
     </View>
@@ -279,13 +292,15 @@ export default function DiceRoller() {
     <Text style={diceStyles.poolLabel}>Results:</Text>
     <View style={diceStyles.resultsWrapper}>
       {results.map((img, i) => (
-        <Image key={i} source={img} style={diceStyles.resultIcon} />
+        <Image key={i} source={img.src} style={diceStyles.resultIcon} />
       ))}
     </View>
   </View>
   
 </ScrollView>
 
+    </View>
+    </View>
     </View>
   );
 }
