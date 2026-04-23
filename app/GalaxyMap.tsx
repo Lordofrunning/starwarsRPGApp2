4a2175eb06331661babeae4beb6fba6c907136a4
tyler <tylerchipman09@gmail.com>
2026-04-23 14:24:03 -0600

    galaxy togle

--- app/GalaxyMap.tsx
+++ app/GalaxyMap.tsx
@@ -1,14 +1,14 @@
 import { Stack, useRouter } from 'expo-router';
 import React, { useState } from 'react';
 import {
-  Alert,
-  Dimensions,
-  Image,
-  Modal,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View
+    Alert,
+    Dimensions,
+    Image,
+    Modal,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
 } from 'react-native';
 import ImageZoom from 'react-native-image-pan-zoom';
 import { useTheme } from './ThemeContext';
